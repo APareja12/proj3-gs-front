c4df23a9ea6801b7f35c69c024a3395888370ae7
apareja <alexis.pareja@hotmail.com>
2021-10-29 09:46:03 -0400

    wip: Adds Favorites variables

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,34 +1,43 @@
 import { useState } from "react";
 
 function Favorites(props) {
-//   const id = props.match.params.id;
-//   const films = props.films;
-//   const film = films.find(p => p._id === id);
+  const id = props.match.params.id;
+  const films = props.films;
+  const film = films.find(p => p._id === id);
 
-//   const [ editForm, setEditForm ] = useState(film);
+  const [ editForm, setEditForm ] = useState(film);
 
-//   const handleChange = event => {
-//     setEditForm(prevState => ({
-//         ...prevState,
-//         [event.target.name]: event.target.value
-//     }));
-// }
+  const handleChange = event => {
+    setEditForm(prevState => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+    }));
+}
 
-// const handleSubmit = event => {
-//     event.preventDefault();
-//     const { _id, name, title, image } = editForm;
-//     props.updatePeople({ name, title, image }, _id);
+const handleSubmit = event => {
+    event.preventDefault();
+    const { _id, name, title, image } = editForm;
+    props.updateFilms({ name, title, image }, _id);
     
-// }
+}
 
-// const removePerson = () => {
-//     props.deletePeople(person._id);
-//     props.history.push('/');
-// }
+const removeFilm = () => {
+    props.deleteFilms(film._id);
+    props.history.push('/');
+}
 
 
 
-    return <h1>Favorites</h1>;
-  }
+    return (
+      <div className="film">
+        <h1>{film.title}</h1>
+        <h4>{film.year}</h4>
+        <h4>{film.director}</h4>
+        <h4>{film.country}</h4>
+        <button id="delete" onClick={removeFilm}>DELETE</button>
+        <form action=""></form>
+      </div>
+    );
+    }
   
   export default Favorites;
