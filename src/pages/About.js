bebe97f88fa41056b9376bb5367f6afb6be5ce66
apareja <alexis.pareja@hotmail.com>
2021-10-31 19:09:22 -0400

    adds react helmet

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,19 @@
 function About(props) {
   return (
+    <>
     <h1>About</h1>
-    
+    <p>I'm baby cornhole veniam ut williamsburg marfa raw denim thundercats. Shaman labore cred art party 90's cold-pressed kale chips brooklyn. Ipsum tofu occupy VHS ex, viral intelligentsia est culpa cardigan normcore organic deserunt vegan. Taxidermy keytar deserunt four dollar toast church-key tousled. Live-edge vegan actually synth pop-up.
+
+Blog keffiyeh adaptogen aute drinking vinegar gluten-free, chambray bushwick la croix pour-over. Waistcoat you probably haven't heard of them hell of adaptogen jianbing actually single-origin coffee ex gochujang pop-up drinking vinegar kitsch sartorial veniam. Kitsch lumbersexual master cleanse, ut vinyl fugiat ex seitan snackwave four dollar toast hexagon cupidatat fixie iPhone. Tattooed gastropub master cleanse culpa.
+
+Sartorial gluten-free crucifix lomo, farm-to-table gentrify mollit put a bird on it vice cardigan swag pork belly. Commodo locavore nisi voluptate. Fashion axe sint voluptate cardigan ipsum YOLO intelligentsia PBR&B brunch 3 wolf moon plaid nostrud. Non id ad, anim farm-to-table street art do ut commodo sunt squid edison bulb kogi master cleanse. Taiyaki synth swag enamel pin, tofu cray poutine gastropub sriracha.
+
+Retro seitan magna velit hashtag taiyaki poutine leggings 3 wolf moon XOXO church-key. Irony consequat post-ironic, sint aesthetic tote bag godard labore velit edison bulb meggings humblebrag cornhole occaecat roof party. Est kale chips semiotics godard PBR&B snackwave. Hoodie voluptate dreamcatcher normcore tumblr tousled, nisi cray ipsum meh. Salvia cred nulla gastropub occupy pariatur la croix yuccie, pitchfork literally.
+
+Beard roof party ipsum la croix. Chicharrones you probably haven't heard of them hoodie man braid cornhole. Mustache synth put a bird on it, yuccie activated charcoal stumptown velit banjo lumbersexual tattooed scenester hexagon pop-up. Wayfarers lyft raw denim gochujang VHS ullamco cronut id echo park deep v readymade helvetica. Truffaut taiyaki sartorial ad yuccie crucifix. Raclette culpa iceland live-edge.
+
+Dummy text? More like dummy thicc text, amirite?</p>
+    </>
     )
    
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet'
 import  Form  from '../components/Form'
 import { Link } from "react-router-dom";
 import { StyledMain } from '../styles';
@@ -5,7 +6,12 @@ import '../index.css';
 
 const Home = (props) => {
    return (
+      <>
+      <Helmet>
+         <title>Welcome to Golden Silence 🌟 </title>
+      </Helmet>
    <h1>Search for a Silent Film </h1>
+   </>
    )
   }
   
