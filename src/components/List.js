bbb74a938af473df3d02247100a39d68ef3594ce
apareja <alexis.pareja@hotmail.com>
2021-11-01 19:32:47 -0400

    adds search function

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+
+
+ const List({films}) {
+    return (
+        <div>  
+            <ul>
+            {films.map(film => (
+                <li key={film.id}>
+                    Title:
+                       <span>{film.title}</span>
+                    Year: 
+                       <span>{film.year}</span>
+                </li>   
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+export default List;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 import { Helmet } from 'react-helmet'
+import { useState, useEffect } from 'react'
+import List from './components/List'
 import '../index.css';
 
 const Home = (props) => {
+   const [films, setFilms] = useState([])
+   const [search, setSearch] = useState('')
+   
    return (
       <>
       <Helmet>
          <title>Welcome to Golden Silence 🌟 </title>
       </Helmet>
    <h1>Search for a Silent Film </h1>
-   <input id="search-input" type="text" />
+   <input id="search-input"
+          type="text"
+          placeholder="Search name" 
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+           />
    <button id="search-btn">Search</button>
+   <List films={films}/>
    </>
    )
   }
