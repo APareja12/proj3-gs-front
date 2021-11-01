b39e36937b6ba8eae2ce1e3379c8a3ee4d69ba64
apareja <alexis.pareja@hotmail.com>
2021-11-01 13:19:55 -0400

    fixes app url

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ function App() {
 
   const fetchData = useRef(null);
 
-  const URL = "http://localhost:3001/films/" 
+  // const URL = "http://localhost:3001/films/" 
 
-  // const URL = "https://golden-silence-api.herokuapp.com/films/"
+  const URL = "https://golden-silence-api.herokuapp.com/films/"
 
 
   const getFilms = async () => {
